0d8ab8e5c98f9c5683f7ce22992c060daca1f77d
Ruben Torres <rubentdlh@gmail.com>
2014-11-22 15:08:51 -0300

    Add dummy search function

--- js/app.js
+++ js/app.js
@@ -4,6 +4,21 @@
 
 	app.controller('SearchController', ['$http', '$scope', function($http, $scope){
 		this.results = [];
+		var search = this;
+
+		this.find = function(){
+			var query = $("#search_query").val();
+
+			search.results = [];
+			if(query != undefined && query.length > 0){
+				$http.get('data/results.json')
+				.success( function(data){
+					search.results = data;
+				});
+			}
+
+		}
+
 	}]); 
 	
 	app.directive("navigation", function(){
