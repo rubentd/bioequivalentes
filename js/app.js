31477a99bf980466b1724d4e40480604239f6452
farmahorro <farmahorro@agba.dreamhost.com>
2014-11-22 11:40:06 -0800

    Probably reading the data from the database

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 (function() {
 
 	var app = angular.module('farmAhorro', []);
+	var db = openDatabase('bioequivalents', '1.0', 'bioequivalent medicine', 1024 * 1024 * 1024);
 
 	app.controller('SearchController', ['$http', '$scope', function($http, $scope){
 		this.results = [];
@@ -9,20 +10,41 @@
 
 		var search = this;
 
+		this.init = function(){
+
+			db.transaction(function (tx) {
+				tx.executeSql('DROP TABLE IF EXISTS bioequivalence', [], function(){
+					tx.executeSql('CREATE TABLE bioequivalence ( id unique, usage TEXT, active_ingredient TEXT, reference_product TEXT, reference_product_lab TEXT, bioequivalent_product TEXT, register TEXT, bioequivalent_lab TEXT, resolution TEXT, date TEXT)', [], function(){
+						$http.get('data/results.json')
+						.success( function(data){
+							db.transaction(function (tx) {
+								$.each(data, function(index, value) {
+									tx.executeSql("INSERT INTO bioequivalence (id, active_ingredient, bioequivalent_product, register, bioequivalent_lab, resolution, date, usage) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", value);
+								});
+							});
+						});
+					});
+				});
+			});
+		};
+
 		this.find = function(){
 			var query = $("#search_query").val();
-
 			search.results = [];
 			if(query != undefined && query.length > 0){
-				$http.get('data/results.json')
-				.success( function(data){
-					search.results = data;
+				db.transaction(function(tx) {
+					tx.executeSql('SELECT * FROM bioequivalence WHERE bioequivalent_product like ?', ['%' + query + '%'], function(tx, results){
+						for (var i=0; i < results.rows.length; i++){
+							row = results.rows.item(i);
+							search.results.push(row);
+						 }
+					});
 				});
 			}
 		};
 
 		this.getDetails = function(id){
-			
+
 			$('#listing').addClass('slide-left');
 			$('#details').removeClass('slide-right');
 
@@ -36,9 +58,9 @@
 			$('#listing').removeClass('slide-left');
 			$('#details').addClass('slide-right');
 		}
+        this.init();
+	}]);
 
-	}]); 
-	
 	app.directive("navigation", function(){
 		return {
 			restrict: 'E',
@@ -61,4 +83,3 @@
 	});
 
 })();
-
