cde20f94792a68b4e9d582cb995e23c8cd43458b
Ruben Torres <rubentdlh@gmail.com>
2014-11-22 16:21:03 -0300

    ADd fancy animation and view details

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@
 
 	app.controller('SearchController', ['$http', '$scope', function($http, $scope){
 		this.results = [];
+		this.mode = 'listing';
+		this.productDetails = {};
+
 		var search = this;
 
 		this.find = function(){
@@ -16,7 +19,22 @@
 					search.results = data;
 				});
 			}
+		};
 
+		this.getDetails = function(id){
+			
+			$('#listing').addClass('slide-left');
+			$('#details').removeClass('slide-right');
+
+			this.productDetails = {
+				'reference_product': 'Zolven',
+				'reference_product_lab': 'Novartis'
+			};
+		}
+
+		this.goToListing = function(){
+			$('#listing').removeClass('slide-left');
+			$('#details').addClass('slide-right');
 		}
 
 	}]); 
@@ -25,6 +43,20 @@
 		return {
 			restrict: 'E',
 			templateUrl: 'navigation.html'
+		};
+	});
+
+	app.directive("searchBar", function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'search-bar.html'
+		};
+	});
+
+	app.directive("staticInfo", function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'static-info.html'
 		};
 	});
 
